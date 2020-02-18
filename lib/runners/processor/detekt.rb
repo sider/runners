d6c32eef03b186d9d8443ab26c0d2a9b10a143c1
hiroko.ozaki <hiroko.ozaki@fixstars.com>
2020-02-18 16:43:42 +0900

    WIP

---
+++ lib/runners/processor/detekt.rb
@@ -0,0 +1,541 @@
+module Runners
+  class Processor::Detekt < Processor
+    include Java
+    require 'active_support/core_ext'
+
+    Schema = StrongJSON.new do
+
+      def self.array_or?(type)
+        array = array(type)
+        enum?(type, array, detector: -> (value) {
+          case value
+          when Array
+            array
+          else
+            type
+          end
+        })
+      end
+
+      def self.array_or(type)
+        array = array(type)
+
+        enum(type, array, detector: -> (value) {
+          case value
+          when Array
+            array
+          else
+            type
+          end
+        })
+      end
+
+      let :reporter, enum(literal("json"), literal("plain"), literal("checkstyle"))
+
+      let :cli_config, object(
+        auto_correct: boolean?,
+        baseline_path: string?,
+        classpath: array?(string),
+        config: array?(string),
+        config_resource: array?(string),
+        disable_default_rulesets: boolean?,
+        excludes: array?(string),
+        includes: array?(string),
+        input: array?(string),
+        language_version: number?,
+        jvm_target: number?,
+        plugins: array?(string),
+        report_id: string?,
+        report_path: string?
+      )
+
+      let :gradle_config, object(
+        task: string,
+        reporter: reporter,
+        output: string?
+      )
+
+      let :maven_config, object(
+        goal: string
+      )
+
+      let :cli, Schema::RunnerConfig.base.update_fields {|hash| hash[:cli] = cli_config}
+      let :gradle, Schema::RunnerConfig.base.update_fields {|hash| hash[:gradle] = gradle_config}
+      let :maven, Schema::RunnerConfig.base.update_fields {|hash| hash[:maven] = maven_config }
+      let :base, Schema::RunnerConfig.base
+
+      let :runner_config, enum(
+        cli, gradle, maven, base,
+        detector: -> (value) {
+          case
+          when value.key?(:cli)
+            cli
+          when value.key?(:gradle)
+            gradle
+          when value.key?(:maven)
+            maven
+          else
+            base
+          end
+        }
+      )
+    end
+
+    def self.ci_config_section_name
+      'detekt'
+    end
+
+    def detekt_config
+      @detekt_config
+    end
+
+    def cli_config
+      detekt_config[:cli]
+    end
+
+    def gradle_config
+      detekt_config[:gradle]
+    end
+
+    def maven_config
+      detekt_config[:maven]
+    end
+
+    def run_cli
+      args = []
+
+      if cli_config[:auto_correct]
+        args.push("--auto-correct")
+      end
+
+      unless cli_config[:baseline_path].nil?
+        args = check_user_file_exists(
+          [cli_config[:baseline_path]],
+          "baseline",
+          args
+        )
+      end
+
+      unless cli_config[:classpath].empty?
+        args = check_user_file_exists(
+          cli_config[:classpath],
+          "classpath",
+          args
+        )
+      end
+
+      unless cli_config[:config].empty?
+        args = check_user_file_exists(
+          cli_config[:config],
+          "config",
+          args
+        )
+      end
+
+      unless cli_config[:config_resource].empty?
+        args = check_user_file_exists(
+          cli_config[:config_resource],
+          "config_resource",
+          args
+        )
+      end
+
+      if cli_config[:disable_default_rulesets]
+        args.push("--disable-default-rulesets")
+      end
+
+      unless cli_config[:excludes].empty?
+        args.push("--excludes", cli_config[:excludes].join(","))
+      end
+
+      unless cli_config[:includes].empty?
+        args.push("--includes", cli_config[:includes].join(","))
+      end
+
+      unless cli_config[:input].empty?
+        args = check_user_file_exists(
+          cli_config[:input],
+          "input",
+          args
+        )
+      end
+
+      unless cli_config[:language_version].nil?
+        args.push("--language-version", cli_config[:language_version].to_s)
+      end
+
+      unless cli_config[:jvm_target].nil?
+        args.push("--jvm-target", cli_config[:jvm_target].to_s)
+      end
+
+      unless cli_config[:plugins].empty?
+        args.push("--plugins", cli_config[:plugins].join(","))
+      end
+
+      report_id = cli_config[:report_id]
+      report_path = cli_config[:report_path]
+      output_file_path = current_dir / report_path
+      args.push("--report", "#{report_id}:#{output_file_path.to_path}")
+
+      capture3(analyzer_bin, *args)
+
+      if output_file_path.exist?
+        trace_writer.message "Reading output from #{report_path}..."
+        output = output_file_path.read
+      else
+        msg = "#{report_path} does not exist because an unexpected error occurred!"
+        trace_writer.error msg
+        raise msg
+      end
+
+      construct_checkstyle_result(output)
+    end
+
+    def run_gradle
+      stdout, = capture3("./gradlew", "--quiet", gradle_config[:task])
+
+      output_file = current_dir / "build/reports/detekt.txt"
+
+      # output = if output_file
+      #            trace_writer.message "Reading output from #{output_file}..."
+      #            output_file.read
+      #          else
+      #            stdout
+      #          end
+
+      output = stdout
+
+      # construct_result(gradle_config[:reporter], output)
+      trace_writer.message "*********************"
+      trace_writer.message output.to_yaml
+      trace_writer.message "*********************"
+      construct_plain_result(output, true)
+    end
+
+    def run_maven
+      report_path, phase = check_maven_config()
+
+      unless maven_config[:goal].nil?
+        goal = maven_config[:goal]
+      else
+        goal = phase
+      end
+
+      mvn_options = %w[--quiet --batch-mode -Dmaven.test.skip=true -Dmaven.main.skip=true]
+
+      # NOTE: `mvn` fails when issues are found, so we should not check the exit status.
+      capture3("mvn", goal, *mvn_options)
+
+      output_file_path = current_dir / report_path
+
+      if output_file_path.exist?
+        trace_writer.message "Reading output from #{report_path}..."
+        output = output_file_path.read
+      else
+        msg = "#{report_path} does not exist because an unexpected error occurred!"
+        trace_writer.error msg
+        raise msg
+      end
+
+      # construct_result(maven_config[:reporter], output)
+      construct_checkstyle_result(output)
+    end
+
+    def analyzer_version
+      unknown_version = "0.0.0"
+
+      @analyzer_version ||=
+        case
+        when gradle_config
+          extract_detekt_version_for_gradle || unknown_version
+        when maven_config
+          extract_detekt_version_for_maven(current_dir) || unknown_version
+        when cli_config
+          pom_dir = Pathname(ENV['RUNNER_USER_HOME']) / analyzer_name
+          extract_detekt_version_for_maven(pom_dir) || unknown_version
+        else
+          unknown_version
+        end
+    end
+
+    def extract_detekt_version_for_gradle
+      stdout, = capture3!("./gradlew", "--quiet", "dependencies")
+      stdout.each_line do |line|
+        line.match(/io\.gitlab\.arturbosch\.detekt:detekt-cli:([\d\.]+)/) do |match|
+          return match.captures.first
+        end
+      end
+    end
+
+    def extract_detekt_version_for_maven(pom_dir)
+      pom_file = pom_dir / "pom.xml"
+      return unless pom_file.exist?
+
+      groupId = "io.gitlab.arturbosch.detekt"
+      artifactId = "detekt-cli"
+      pom = REXML::Document.new(pom_file.read)
+      pom.root.each_element("//dependency/groupId[text()='#{groupId}']") do |element|
+        dependency = element.parent
+        if dependency.elements["artifactId"].text == artifactId
+          return dependency.elements["version"].text
+        end
+      end
+    end
+
+    def analyzer_name
+      'detekt'
+    end
+
+    def analyze(changes)
+      ensure_runner_config_schema(Schema.runner_config) do |config|
+        delete_unchanged_files changes, only: [".kt", ".kts"]
+
+        check_runner_config(config) do |checked_config|
+          @detekt_config = checked_config
+
+          issues = case
+                   when gradle_config
+                     run_gradle
+                   when maven_config
+                     run_maven
+                   else
+                     run_cli
+                   end
+
+          Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
+            issues.each do |issue|
+              result.add_issue issue
+            end
+          end
+        end
+      end
+    end
+
+    def construct_result(reporter, output)
+      case reporter
+      when "json"
+        construct_json_result(output)
+      when "checkstyle"
+        construct_checkstyle_result(output)
+      when "plain"
+        construct_plain_result(output)
+      end
+    end
+
+    def construct_issue(file:, line:, message:, rule: "")
+      Issue.new(
+        path: relative_path(working_dir.realpath / file, from: working_dir.realpath),
+        location: Location.new(start_line: line),
+        id: rule.empty? ? Digest::SHA1.hexdigest(message)[0, 8] : rule,
+        message: message,
+      )
+    end
+
+    def construct_json_result(output)
+      json = JSON.parse(output, symbolize_names: true)
+
+      # json = Hash.from_xml(output)
+
+      trace_writer.message "jjjjjjjjjjjjjjjjjjjjjjjjj"
+      trace_writer.message json.to_yaml
+
+
+      json.flat_map do |hash|
+        trace_writer.message "***************************"
+        trace_writer.message hash.to_yaml
+        trace_writer.message "***************************"
+        hash[:file].flat_map do |error|
+          race_writer.message error.to_yaml
+          trace_writer.message "***************************"
+          construct_issue(
+            file: hash[:file],
+            line: (error[:line]).to_i,
+            message: error[:message],
+            rule: error[:rule],
+          )
+          trace_writer.message "***************************"
+        end
+      end
+    end
+
+    def construct_checkstyle_result(output)
+      document = REXML::Document.new(output)
+      unless document.root
+        msg = "Invalid XML output!"
+        trace_writer.error msg
+        raise msg
+      end
+
+      issues = []
+
+      document.root.each_element("file") do |file|
+        file.each_element do |error|
+          case error.name
+          when "error"
+            issues << construct_issue(
+              file: file[:name],
+              line: error[:line],
+              message: error[:message],
+              rule: error[:source],
+            )
+          when "exception"
+            add_warning error.text.strip, file: file[:name]
+          end
+        end
+      end
+
+      issues
+    end
+
+    def construct_plain_result(output, is_stdout = false)
+      regex = /(.*) at \/(.*):(.*):(.*) - (.*)/
+      if is_stdout
+        regex = /(.*) at \/(.*):(.*):(.*)/
+      end
+
+      output.lines(chomp: true).map do |line|
+        trace_writer.message "********************* line"
+        trace_writer.message line
+        trace_writer.message "********************* line"
+
+        match = line.match(regex)
+
+
+        unless match.nil?
+          rule, file, line, _col, message = match.captures
+          construct_issue(
+            rule: rule,
+            file: file,
+            line: line,
+            message: message,
+          )
+        else
+          raise "Unexpected line: #{line.inspect}"
+        end
+      end
+    end
+
+    def check_user_file_exists(paths, option_name, args)
+      ret = args.dup
+      exists = []
+
+      paths.each do |a_path|
+        path = current_dir / a_path
+        if path.exist?
+          exists.push(a_path)
+        end
+      end
+
+      unless exists.empty?
+        ret.push("--#{option_name}", exists.join(","))
+      else
+        msg = "Given '#{option_name}' does not exist. Run without adding '--#{option_name}' option."
+        trace_writer.error msg
+      end
+
+      return ret
+    end
+
+    def check_maven_config()
+      report_path = nil
+      phase = "verify"
+
+      pom_file = current_dir / "pom.xml"
+      unless pom_file.exist?
+        msg = "'pom.xml' does not exist. Processing cannot continue."
+        trace_writer.error msg
+        raise msg
+      end
+      
+      pom = REXML::Document.new(pom_file.read)
+      id = "detekt"
+
+      fds = pom.get_elements("//project/build/plugins/plugin/executions/execution/id[text()='#{id}']").first
+      unless fds.nil?
+        dom_setting = fds.parent
+        unless dom_setting.elements["phase"].nil?
+          phase = dom_setting.elements["phase"]
+        end
+
+        fdsp = dom_setting.get_elements("//configuration/target/java/arg[@value='--report']").first
+        unless fdsp.nil?
+          dom_setting_report = fdsp.next_element.attribute("value").to_s.split(/:/)
+          report_path = dom_setting_report[1]
+        end
+      end
+
+      if report_path.nil?
+        msg = "arg '--report' undefined on 'pom.xml'. Processing cannot continue."
+        trace_writer.error msg
+        raise msg
+      end
+
+      return report_path, phase
+    end
+
+    def check_runner_config(config)
+      case
+      when config[:gradle]
+        yield(
+          {
+            gradle: {
+              task: config[:gradle][:task],
+              reporter: config[:gradle][:reporter],
+              output: config[:gradle][:output]
+            }
+          }
+        )
+      when config[:maven]
+        yield(
+          {
+            maven: {
+              goal: config[:maven][:goal],
+            }
+          }
+        )
+      when config[:cli]
+        yield(
+          {
+            cli: {
+              auto_correct: config[:cli][:auto_correct] || false,
+              baseline_path: config[:cli][:baseline_path],
+              classpath: Array(config[:cli][:classpath]) || [],
+              config: Array(config[:cli][:config]) || [],
+              config_resource: config[:cli][:config_resource] || [],
+              disable_default_rulesets: config[:cli][:disable_default_rulesets] || false,
+              excludes: Array(config[:cli][:excludes]) || [],
+              includes: Array(config[:cli][:includes]) || [],
+              input: Array(config[:cli][:input]) || [],
+              language_version: config[:cli][:language_version],
+              jvm_target: config[:cli][:jvm_target],
+              plugins: Array(config[:cli][:plugins]) || [],
+              report_id: config[:cli][:report_id] || "xml",
+              report_path: config[:cli][:report_path] || "detekt_cli_report.xml"
+            }
+          }
+        )
+      else
+        yield(
+          {
+            cli: {
+              auto_correct: false,
+              baseline_path: nil,
+              classpath: [],
+              config: [],
+              config_resource: [],
+              disable_default_rulesets: false,
+              excludes: [],
+              includes: [],
+              input: [],
+              language_version: nil,
+              jvm_target: nil,
+              plugins: [],
+              report_id: "xml",
+              report_path: "detekt_cli_report.xml"
+            }
+          }
+        )
+      end
+    end
+  end
+end
---
+++ test/smokes/detekt/expectations.rb
@@ -0,0 +1,261 @@
+Smoke = Runners::Testing::Smoke
+
+Smoke.add_test(
+  "cli",
+  {
+    guid: "test-guid",
+    timestamp: :_,
+    type: "success",
+    analyzer: { name: "detekt", version: "1.5.1" },
+    issues: [
+      {
+        id: "detekt.EmptyClassBlock",
+        path: "src/FilteredClass.kt",
+        location: { start_line: 2 },
+        message: "The class or object FilteredClass is empty.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.ForEachOnRange",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 44 },
+        message: "Using the forEach method on ranges has a heavy performance cost. Prefer using simple for loops.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.FunctionOnlyReturningConstant",
+        path: "src/App.kt",
+        location: { start_line: 8 },
+        message: "get is returning a constant. Prefer declaring a constant instead.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.MagicNumber",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 44 },
+        message: "This expression contains a magic number. Consider defining it to a well named constant.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.MagicNumber",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 48 },
+        message: "This expression contains a magic number. Consider defining it to a well named constant.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.NestedBlockDepth",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 9 },
+        message: "Function complex is nested too deeply.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.TooGenericExceptionCaught",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 19 },
+        message: "Caught exception is too generic. Prefer catching specific exceptions to the case that is currently handled.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.TooGenericExceptionCaught",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 22 },
+        message: "Caught exception is too generic. Prefer catching specific exceptions to the case that is currently handled.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.TooGenericExceptionCaught",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 34 },
+        message: "Caught exception is too generic. Prefer catching specific exceptions to the case that is currently handled.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+    ]
+  }
+)
+
+Smoke.add_test(
+  "cli_with_options",
+  {
+    guid: "test-guid",
+    timestamp: :_,
+    type: "success",
+    analyzer: { name: "detekt", version: "1.5.1" },
+    issues: [
+      {
+        id: "detekt.EmptyClassBlock",
+        path: "src/FilteredClass.kt",
+        location: { start_line: 2 },
+        message: "The class or object FilteredClass is empty.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.ForEachOnRange",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 44 },
+        message: "Using the forEach method on ranges has a heavy performance cost. Prefer using simple for loops.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.FunctionOnlyReturningConstant",
+        path: "src/App.kt",
+        location: { start_line: 8 },
+        message: "get is returning a constant. Prefer declaring a constant instead.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.NestedBlockDepth",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 9 },
+        message: "Function complex is nested too deeply.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+    ]
+  }
+)
+
+# Smoke.add_test(
+#   "gradle-plain-task",
+#   {
+#     guid: "test-guid",
+#     timestamp: :_,
+#     type: "success",
+#     analyzer: { name: "detekt", version: "1.5.1" },
+#     issues: [
+#       {
+#         id: "0ebc0f91",
+#         path: "src/main/kotlin/gradle/App.kt",
+#         location: { start_line: 9 },
+#         message: "Unexpected indentation (12) (it should be 6) (cannot be auto-corrected)",
+#         links: [],
+#         object: nil,
+#         git_blame_info: nil
+#       },
+#       {
+#         id: "5e2630c4",
+#         path: "src/main/kotlin/gradle/App.kt",
+#         location: { start_line: 8 },
+#         message: "Unexpected indentation (2) (it should be 4) (cannot be auto-corrected)",
+#         links: [],
+#         object: nil,
+#         git_blame_info: nil
+#       }
+#     ]
+#   }
+# )
+
+# Smoke.add_test(
+#   "gradle_ktlint-gradle-plugin",
+#   {
+#     guid: "test-guid",
+#     timestamp: :_,
+#     type: "success",
+#     analyzer: { name: "ktlint", version: "0.34.0" },
+#     issues: [
+#       {
+#         id: "experimental:indent",
+#         path: "src/main/kotlin/gradle/App.kt",
+#         location: { start_line: 10 },
+#         message: "Unexpected indentation (6) (should be 4)",
+#         links: [],
+#         object: nil,
+#         git_blame_info: nil
+#       },
+#       {
+#         id: "indent",
+#         path: "src/main/kotlin/gradle/App.kt",
+#         location: { start_line: 10 },
+#         message: "Unexpected indentation (6) (it should be 8) (cannot be auto-corrected)",
+#         links: [],
+#         object: nil,
+#         git_blame_info: nil
+#       }
+#     ]
+#   }
+# )
+
+Smoke.add_test(
+  "maven",
+  {
+    guid: "test-guid",
+    timestamp: :_,
+    type: "success",
+    analyzer: { name: "detekt", version: "1.5.1" },
+    issues: [
+      {
+        id: "detekt.EmptyClassBlock",
+        path: "src/FilteredClass.kt",
+        location: { start_line: 2 },
+        message: "The class or object FilteredClass is empty.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.ForEachOnRange",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 44 },
+        message: "Using the forEach method on ranges has a heavy performance cost. Prefer using simple for loops.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.FunctionOnlyReturningConstant",
+        path: "src/App.kt",
+        location: { start_line: 8 },
+        message: "get is returning a constant. Prefer declaring a constant instead.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+      {
+        id: "detekt.NestedBlockDepth",
+        path: "src/ComplexClass.kt",
+        location: { start_line: 9 },
+        message: "Function complex is nested too deeply.",
+        links: [],
+        object: nil,
+        git_blame_info: nil
+      },
+    ]
+  }
+)
+
+Smoke.add_test(
+  "broken_sider_yml",
+  {
+    guid: "test-guid",
+    timestamp: :_,
+    type: "failure",
+    analyzer: nil,
+    message: "Invalid configuration in `sider.yml`: unknown attribute at config: `$.linter.detekt`"
+  }
+)
