47982d7b756a10d095876173205fc9c8fb11784d
Yutaka Kamei <yutaka@sider.review>
2020-04-03 20:50:50 +0900

    Fix test_git_ssh_path_is_pathname
    We deleted the repository sider/go_private_library.
    It's not necessary to use a private repository in this test,
    so, I set a deploy key on sider/runners.

--- test/smokes/gometalinter/expectations.rb
+++ test/smokes/gometalinter/expectations.rb
@@ -15,8 +15,7 @@ Smoke.add_test(
         DEPRECATION WARNING!!!
         The support for Go Meta Linter is deprecated. Sider will drop these versions on April 30, 2020.
         Please consider using an alternative tool GolangCI-Lint. See https://github.com/alecthomas/gometalinter/issues/590
-      MSG
-        .strip,
+      MSG.strip,
       file: "sider.yml"
     }
   ]
@@ -43,28 +42,6 @@ Smoke.add_test(
   },
   warnings: [{ message: /The support for Go Meta Linter is deprecated/, file: "sideci.yml" }]
 )
-
-Smoke.add_test(
-  "private_dependency",
-  {
-    guid: "test-guid",
-    timestamp: :_,
-    type: "success",
-    issues: [
-      {
-        path: "main.go",
-        location: { start_line: 13 },
-        id: "bc596b8e1c54fbda9055c3f53940b5df39d6c11f",
-        message: "[vet] Printf call has arguments but no formatting directives",
-        links: [],
-        object: nil,
-        git_blame_info: nil
-      }
-    ],
-    analyzer: { name: "Go Meta Linter", version: "2.0.11" }
-  },
-  warnings: [{ message: /The support for Go Meta Linter is deprecated/, file: "sideci.yml" }]
-) { |config| config.ssh_key = "ssh_key" }
 
 Smoke.add_test(
   "specify_config",
