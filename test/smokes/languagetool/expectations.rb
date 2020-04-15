c18943f8b6dee249f767bd533f696276497df597
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-15 18:10:05 +0900

    Add smoke test case for `enable_categories` option

--- test/smokes/languagetool/expectations.rb
+++ test/smokes/languagetool/expectations.rb
@@ -267,3 +267,24 @@ Smoke.add_test(
     analyzer: { name: "LanguageTool", version: "4.9" }
   }
 )
+
+Smoke.add_test(
+  "option_enable_categories",
+  {
+    guid: "test-guid",
+    timestamp: :_,
+    type: "success",
+    issues: [
+      {
+        id: "UPPERCASE_SENTENCE_START",
+        path: "sample.txt",
+        location: { start_line: 1 },
+        message: "This sentence does not start with an uppercase letter",
+        links: [],
+        object: { sentence: "this is a.", type: "typographical", category: "CASING", replacements: %w[This] },
+        git_blame_info: nil
+      }
+    ],
+    analyzer: { name: "LanguageTool", version: "4.9" }
+  }
+)
