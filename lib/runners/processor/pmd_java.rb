f97c5b6c522fe92d8664701d4a03bbbe9efdb0b6
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-04-30 15:20:39 +0900

    Use `-reportfile` option

--- lib/runners/processor/pmd_java.rb
+++ lib/runners/processor/pmd_java.rb
@@ -36,10 +36,11 @@ module Runners
 
     private
 
-    def cli_args
+    def cli_args(report_file)
       [].tap do |args|
         args << "-language" << "java"
         args << "-format" << "xml"
+        args << "-reportfile" << report_file
         args << "-dir" << dir
         args << "-rulesets" << rulesets.join(",")
         min_priority&.tap { args << "-minimumpriority" << _1.to_s }
@@ -48,27 +49,39 @@ module Runners
     end
 
     def run_analyzer
-      stdout, stderr, status = capture3(analyzer_bin, *cli_args)
+      report_file = Tempfile.create(["pmd-report-", ".xml"]).path
+
+      _, stderr, status = capture3(analyzer_bin, *cli_args(report_file))
 
       if status.success? || status.exitstatus == 4
+        stderr.each_line do |line|
+          case line
+          when /WARNING: This analysis could be faster, please consider using Incremental Analysis/
+            # We cannot support "incremental analysis" for now. So, ignore it.
+          when /WARNING: (.+)$/
+            add_warning $1
+          end
+        end
+
         Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
-          construct_result(result, stdout, stderr)
+          xml = read_output_xml(report_file)
+          construct_result(xml) { result.add_issue _1 }
         end
       else
         Results::Failure.new(guid: guid, analyzer: analyzer, message: "Unexpected error occurred. Please see the analysis log.")
       end
     end
 
-    def construct_result(result, stdout, stderr)
+    def construct_result(xml)
       # https://github.com/pmd/pmd.github.io/blob/8b0c31ff8e18215ed213b7df400af27b9137ee67/report_2_0_0.xsd
 
-      REXML::Document.new(stdout).root.each_element do |element|
+      xml.root.each_element do |element|
         case element.name
         when "file"
           path = relative_path(element[:name])
 
           element.each_element("violation") do |violation|
-            result.add_issue Issue.new(
+            yield Issue.new(
               path: path,
               location: Location.new(
                 start_line: violation[:beginline],
@@ -86,22 +99,10 @@ module Runners
               schema: Schema.issue,
             )
           end
-
         when "error"
           add_warning element[:msg], file: relative_path(element[:filename]).to_s
-
         when "configerror"
           add_warning "#{element[:rule]}: #{element[:msg]}"
-
-        end
-      end
-
-      stderr.each_line do |line|
-        case line
-        when /WARNING: This analysis could be faster, please consider using Incremental Analysis/
-          # We cannot support "incremental analysis" for now. So, ignore it.
-        when /WARNING: (.+)$/
-          add_warning $1
         end
       end
     end
