9c2fe90cac2d0c271c8ce67c841142c7efe85e62
suetsuguk <kentarosuetsugu@gmail.com>
2020-05-21 14:40:00 +0900

    Format code by rufo and write a smoke test more easily (#989)

--- lib/runners/processor/pmd_cpd.rb
+++ lib/runners/processor/pmd_cpd.rb
@@ -5,21 +5,21 @@ module Runners
     Schema = StrongJSON.new do
       let :runner_config, Schema::BaseConfig.base.update_fields { |fields|
         fields.merge!({
-                        minimum_tokens: numeric?,
-                        files: string?,
-                        filelist: string?,
-                        language: string?,
-                        encoding: string?,
-                        skip_duplicate_files: boolean?,
-                        non_recursive: boolean?,
-                        skip_lexical_errors: boolean?,
-                        ignore_annotations: boolean?,
-                        ignore_identifiers: boolean?,
-                        ignore_literals: boolean?,
-                        ignore_usings: boolean?,
-                        no_skip_blocks: boolean?,
-                        skip_blocks_pattern: string?,
-                      })
+                    minimum_tokens: numeric?,
+                    files: string?,
+                    filelist: string?,
+                    language: string?,
+                    encoding: string?,
+                    skip_duplicate_files: boolean?,
+                    non_recursive: boolean?,
+                    skip_lexical_errors: boolean?,
+                    ignore_annotations: boolean?,
+                    ignore_identifiers: boolean?,
+                    ignore_literals: boolean?,
+                    ignore_usings: boolean?,
+                    no_skip_blocks: boolean?,
+                    skip_blocks_pattern: string?,
+                  })
       }
     end
 
@@ -29,7 +29,7 @@ module Runners
     register_config_schema(name: :pmd_cpd, schema: Schema.runner_config)
 
     def analyzer_version
-      @analyzer_version ||= capture3!("show_pmd_version").yield_self { |stdout,| stdout.strip }
+      @analyzer_version ||= capture3!("show_pmd_version").yield_self { |stdout, | stdout.strip }
     end
 
     def analyzer_name
@@ -40,7 +40,7 @@ module Runners
       "cpd"
     end
 
-    def analyze changes
+    def analyze(changes)
       run_analyzer
     end
 
@@ -55,9 +55,8 @@ module Runners
       end
     end
 
-    def construct_result result, stdout, stderr
+    def construct_result(result, stdout, stderr)
       REXML::Document.new(stdout).root.each_element do |element|
-        
         element.each_element("file") do |file|
           path = relative_path(file[:path])
           id = Digest::SHA1.hexdigest(path.to_s)
@@ -82,15 +81,15 @@ module Runners
     end
 
     def option_files
-      config_linter[:files].then { |v| v ? ["--files", v] : ["--files", DEFAULT_FILES]}
+      config_linter[:files].then { |v| v ? ["--files", v] : ["--files", DEFAULT_FILES] }
     end
 
     def option_filelist
-      config_linter[:filelist].then { |v| v ? ["--filelist", v] : []}
+      config_linter[:filelist].then { |v| v ? ["--filelist", v] : [] }
     end
 
     def option_encoding
-      config_linter[:encoding].then { |v| v ? ["--encoding", v] : []}
+      config_linter[:encoding].then { |v| v ? ["--encoding", v] : [] }
     end
 
     def option_minimum_tokens
@@ -100,45 +99,43 @@ module Runners
     end
 
     def option_language
-      config_linter[:language].then { |v| v ? ["--language", v] : []}
+      config_linter[:language].then { |v| v ? ["--language", v] : [] }
     end
 
     def option_skip_duplicate_files
-      config_linter[:skip_duplicate_files].then { |v| v ? ["--skip-duplicate-files"] : []} 
+      config_linter[:skip_duplicate_files].then { |v| v ? ["--skip-duplicate-files"] : [] }
     end
 
-
     def option_non_recursive
-      config_linter[:non_recursive].then { |v| v ? ["--non-recursive"] : []} 
+      config_linter[:non_recursive].then { |v| v ? ["--non-recursive"] : [] }
     end
 
     def option_skip_lexical_errors
-      config_linter[:skip_lexical_errors].then { |v| v ? ["--skip-lexical-errors"] : []} 
+      config_linter[:skip_lexical_errors].then { |v| v ? ["--skip-lexical-errors"] : [] }
     end
 
-
     def option_ignore_annotations
-      config_linter[:ignore_annotations].then { |v| v ? ["--ignore-annotations"] : []} 
+      config_linter[:ignore_annotations].then { |v| v ? ["--ignore-annotations"] : [] }
     end
 
     def option_ignore_identifiers
-      config_linter[:ignore_identifiers].then { |v| v ? ["--ignore-identifiers"] : []} 
+      config_linter[:ignore_identifiers].then { |v| v ? ["--ignore-identifiers"] : [] }
     end
 
     def option_ignore_literals
-      config_linter[:ignore_literals].then { |v| v ? ["--ignore-literals"] : []} 
+      config_linter[:ignore_literals].then { |v| v ? ["--ignore-literals"] : [] }
     end
 
     def option_ignore_usings
-      config_linter[:ignore_usings].then { |v| v ? ["--ignore-usings"] : []} 
+      config_linter[:ignore_usings].then { |v| v ? ["--ignore-usings"] : [] }
     end
 
     def option_no_skip_blocks
-      config_linter[:no_skip_blocks].then { |v| v ? ["--no-skip-blocks"] : []} 
+      config_linter[:no_skip_blocks].then { |v| v ? ["--no-skip-blocks"] : [] }
     end
 
     def option_skip_blocks_pattern
-      config_linter[:skip_blocks_pattern].then { |v| v ? ["--skip-blocks-pattern", v] : []} 
+      config_linter[:skip_blocks_pattern].then { |v| v ? ["--skip-blocks-pattern", v] : [] }
     end
 
     def cli_options
@@ -154,7 +151,7 @@ module Runners
         *option_no_skip_blocks,
         *option_skip_blocks_pattern,
         "--format", "xml",
-        *option_files
+        *option_files,
       ]
     end
   end
