fe44884a662c15a7b972e8e84954729e90c44d46
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-07-20 17:11:33 +0900

    Fix OptionsTest

--- test/options_test.rb
+++ test/options_test.rb
@@ -69,23 +69,21 @@ class OptionsTest < Minitest::Test
   end
 
   def test_options_with_ssh_key
-    with_runners_options_env(source: { head: 'http://example.com/head' },
-                             ssh_key: 'ssh') do
+    with_runners_options_env(ssh_key: 'ssh', source: new_source) do
       options = Runners::Options.new(stdout, stderr)
       assert_equal 'ssh', options.ssh_key
     end
   end
 
   def test_options_without_ssh_key
-    with_runners_options_env(source: { head: 'http://example.com/head' }) do
+    with_runners_options_env(ssh_key: nil, source: new_source) do
       options = Runners::Options.new(stdout, stderr)
       assert_nil options.ssh_key
     end
   end
 
   def test_options_with_outputs
-    with_runners_options_env(source: { head: 'http://example.com/head' },
-                             outputs: %w[stdout stderr]) do
+    with_runners_options_env(outputs: %w[stdout stderr], source: new_source) do
       options = Runners::Options.new(stdout, stderr)
       assert_instance_of(Runners::IO, options.io)
       assert_equal [stdout, stderr], options.io.ios
@@ -93,8 +91,7 @@ class OptionsTest < Minitest::Test
   end
 
   def test_options_with_outputs_s3_url
-    with_runners_options_env(source: { head: 'http://example.com/head' },
-                             outputs: %w[stdout s3://bucket/abc]) do
+    with_runners_options_env(outputs: %w[stdout s3://bucket/abc], source: new_source) do
       s3_mock = Object.new
       stub(s3_mock).write
       stub(s3_mock).flush
@@ -109,7 +106,7 @@ class OptionsTest < Minitest::Test
   end
 
   def test_options_without_outputs
-    with_runners_options_env(source: { head: 'http://example.com/head' }) do
+    with_runners_options_env(outputs: nil, source: new_source) do
       options = Runners::Options.new(stdout, stderr)
       assert_instance_of(Runners::IO, options.io)
       assert_equal [stdout], options.io.ios
