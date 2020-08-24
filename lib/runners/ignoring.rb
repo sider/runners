82be999bfebae465464f106270c591f7d7478e0a
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-24 21:27:07 +0900

    Fix lib/runners/ignoring.rb

--- lib/runners/ignoring.rb
+++ lib/runners/ignoring.rb
@@ -14,7 +14,7 @@ module Runners
 
         # @see https://git-scm.com/docs/git-ls-files
         ignored_files, _ = workspace.shell.capture3!(
-          "git", "ls-files", "--ignored", "--exclude-from", ignore_path,
+          "git", "ls-files", "--ignored", "--exclude-from", gitignore.path,
           trace_stdout: false,
         )
 
