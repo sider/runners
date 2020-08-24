eca5626a69164405ef69dac637727de33f7de2f7
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-24 10:09:15 +0900

    Update and fix signatures

--- lib/runners/schema/options.rb
+++ lib/runners/schema/options.rb
@@ -1,7 +1,7 @@
 module Runners
   module Schema
     Options = _ = StrongJSON.new do
-      # @type self: Types::Options
+      # @type self: Options
 
       let :source, object?(
         head: string,
