a5a0ab9408f9b2910bc644fd2c7c09bd9cc7aed0
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-08-25 21:01:30 +0900

    Refactor `Runners::Options` class
    This refactoring is for a better type-check. See #1215.

--- test/options_test.rb
+++ test/options_test.rb
@@ -3,60 +3,35 @@ require_relative 'test_helper'
 class OptionsTest < Minitest::Test
   include TestHelper
 
+  def setup
+    stub(Runners::IO::AwsS3).stub? { true }
+  end
+
   def test_options_git_source
-    source_params = {
-      head: 'head_commit',
-      base: 'base_commit',
-      git_url: 'https://github.com/foo/bar',
-      git_url_userinfo: 'user:secret',
-      refspec: "+refs/pull/1234/head:refs/remotes/pull/1234/head",
-    }
     with_runners_options_env(source: source_params) do
       options = Runners::Options.new(stdout, stderr)
-      assert_instance_of Runners::Options::GitSource, options.source
-      assert_equal source_params, options.source.to_h
+      assert_equal expected_output, options.source.to_h
     end
   end
 
   def test_options_git_source_without_base
-    source_params = {
-      head: 'head_commit',
-      git_url: 'https://github.com/foo/bar',
-      git_url_userinfo: 'user:secret',
-      refspec: "+refs/pull/1234/head:refs/remotes/pull/1234/head",
-    }
-    with_runners_options_env(source: source_params) do
+    with_runners_options_env(source: source_params.tap { _1.delete(:base) }) do
       options = Runners::Options.new(stdout, stderr)
-      assert_instance_of Runners::Options::GitSource, options.source
-      assert_equal source_params.merge(base: nil), options.source.to_h
+      assert_equal expected_output.merge(base: nil), options.source.to_h
     end
   end
 
   def test_options_git_source_without_userinfo
-    source_params = {
-      head: 'head_commit',
-      base: 'base',
-      git_url: 'https://github.com/foo/bar',
-      refspec: "+refs/pull/1234/head:refs/remotes/pull/1234/head",
-    }
-    with_runners_options_env(source: source_params) do
+    with_runners_options_env(source: source_params.tap { _1.delete(:git_url_userinfo) }) do
       options = Runners::Options.new(stdout, stderr)
-      assert_instance_of Runners::Options::GitSource, options.source
-      assert_equal source_params.merge(git_url_userinfo: nil), options.source.to_h
+      assert_equal expected_output.merge(git_url_userinfo: nil), options.source.to_h
     end
   end
 
-  def test_options_git_source_without_refspecs
-    source_params = {
-      head: 'head_commit',
-      base: 'base',
-      git_url: 'https://github.com/foo/bar',
-      git_url_userinfo: 'user:secret',
-    }
-    with_runners_options_env(source: source_params) do
+  def test_options_git_source_without_refspec
+    with_runners_options_env(source: source_params.tap { _1.delete(:refspec) }) do
       options = Runners::Options.new(stdout, stderr)
-      assert_instance_of Runners::Options::GitSource, options.source
-      assert_equal source_params.merge(refspec: nil), options.source.to_h
+      assert_equal expected_output.merge(refspec: []), options.source.to_h
     end
   end
 
@@ -84,16 +59,12 @@ class OptionsTest < Minitest::Test
 
   def test_options_with_outputs_s3_url
     with_runners_options_env(outputs: %w[stdout s3://bucket/abc], source: new_source) do
-      s3_mock = Object.new
-      stub(s3_mock).write
-      stub(s3_mock).flush
-      stub(s3_mock).flush!
-      mock(Runners::IO::AwsS3).new("s3://bucket/abc", endpoint: nil) { s3_mock }
-      mock.proxy(Runners::IO).new(stdout, s3_mock)
-
       options = Runners::Options.new(stdout, stderr)
       assert_instance_of(Runners::IO, options.io)
-      assert_equal [stdout, s3_mock], options.io.ios
+      assert_equal 2, options.io.ios.size
+      assert_equal stdout, options.io.ios[0]
+      assert_instance_of Runners::IO::AwsS3, options.io.ios[1]
+      assert_equal "s3://bucket/abc", options.io.ios[1].uri
     end
   end
 
@@ -105,11 +76,40 @@ class OptionsTest < Minitest::Test
     end
   end
 
+  def test_options_with_invalid_outputs
+    with_runners_options_env(outputs: ["foo"], source: new_source) do
+      error = assert_raises(ArgumentError) { Runners::Options.new(stdout, stderr) }
+      assert_equal 'Invalid output option: `"foo"`', error.message
+    end
+  end
+
+  private
+
   def stdout
     @stdout ||= StringIO.new
   end
 
   def stderr
     @stderr ||= StringIO.new
+  end
+
+  def source_params
+    {
+      head: 'e07dc104',
+      base: 'a7c6b27c',
+      git_url: 'https://github.com/foo/bar',
+      git_url_userinfo: 'user:secret',
+      refspec: '+refs/pull/1234/head:refs/remotes/pull/1234/head',
+    }
+  end
+
+  def expected_output
+    {
+      head: 'e07dc104',
+      base: 'a7c6b27c',
+      git_url: 'https://github.com/foo/bar',
+      git_url_userinfo: 'user:secret',
+      refspec: ['+refs/pull/1234/head:refs/remotes/pull/1234/head'],
+    }
   end
 end
