3e254af95327498fed46f59157abeb9f87bd58fc
suetsuguk <kentarosuetsugu@gmail.com>
2020-09-28 16:01:44 +0900

    Write messages when cfg file exists or filter is specified

--- lib/runners/processor/cpplint.rb
+++ lib/runners/processor/cpplint.rb
@@ -53,8 +53,15 @@ module Runners
       user_config_filepath = current_dir / 'CPPLINT.cfg'
       recommended_config_filepath = Pathname(Dir.home) / 'sider_recommended_CPPLINT.cfg'
 
-      return if user_config_filepath.exist?
-      return if config_linter[:filter]
+      if user_config_filepath.exist?
+        trace_writer.message "The cpplint configuration file called CPPLINT.cfg exists in the root directory of your repository. The Sider's recommended set of rules is ignored."
+        return
+      end
+
+      if config_linter[:filter]
+        trace_writer.message "The `filter` option in #{config.path_name} is specified. The Sider's recommended set of rules is ignored."
+        return
+      end
 
       trace_writer.message "The cpplint configuration file called CPPLINT.cfg does not exist in the root directory of your repository. Sider uses our recommended set of rules instead."
       FileUtils.copy(recommended_config_filepath, user_config_filepath)
