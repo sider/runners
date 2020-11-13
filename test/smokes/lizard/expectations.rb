ebd5655af4273856aae06678292f3e3378969548
suetsuguk <kentarosuetsugu@gmail.com>
2020-11-13 14:02:10 +0900

    No need to test all output

--- test/smokes/lizard/expectations.rb
+++ test/smokes/lizard/expectations.rb
@@ -30,15 +30,7 @@ s.add_test(
       path: "example.c",
       location: { start_line: 13, end_line: 16 },
       message: "Complexity is 1 for 4 line(s) of code at print_hello_world.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 10,
-        PARAM: 0,
-        length: 4,
-        function: "print_hello_world",
-        long_name: "print_hello_world()"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "64218a0fd55b3bf3c6e2231b24cb97b4beb7c07d", original_line: 13, final_line: 13
@@ -49,15 +41,7 @@ s.add_test(
       path: "src/baz/hello.php",
       location: { start_line: 2, end_line: 6 },
       message: "Complexity is 1 for 4 line(s) of code at hello_world.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 7,
-        PARAM: 0,
-        length: 5,
-        function: "hello_world",
-        long_name: "hello_world ( )"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "1c5f9d44bb3f43d94efe7c31b50c452875698a80", original_line: 2, final_line: 2
@@ -68,15 +52,7 @@ s.add_test(
       path: "src/baz/hello.rb",
       location: { start_line: 1, end_line: 4 },
       message: "Complexity is 1 for 3 line(s) of code at hello_world.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 4,
-        PARAM: 0,
-        length: 4,
-        function: "hello_world",
-        long_name: "hello_world"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "cef9c6f253991b843ddae7253c8b054d2f4bf046", original_line: 1, final_line: 1
@@ -87,15 +63,7 @@ s.add_test(
       path: "src/baz/hello.scala",
       location: { start_line: 2, end_line: 5 },
       message: "Complexity is 1 for 3 line(s) of code at hello_world.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 10,
-        PARAM: 0,
-        length: 4,
-        function: "hello_world",
-        long_name: "hello_world"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "6bbc59d8a3b8d34f9a4b6452b7ec62242797bfec", original_line: 2, final_line: 2
@@ -106,15 +74,7 @@ s.add_test(
       path: "src/baz/qux/hello.lua",
       location: { start_line: 2, end_line: 5 },
       message: "Complexity is 1 for 3 line(s) of code at hello_world.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 6,
-        PARAM: 0,
-        length: 4,
-        function: "hello_world",
-        long_name: "hello_world )"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "1c5f9d44bb3f43d94efe7c31b50c452875698a80", original_line: 2, final_line: 2
@@ -125,15 +85,7 @@ s.add_test(
       path: "src/baz/qux/hello.rs",
       location: { start_line: 2, end_line: 4 },
       message: "Complexity is 1 for 3 line(s) of code at main.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 10,
-        PARAM: 0,
-        length: 3,
-        function: "main",
-        long_name: "main"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "77cf7c960249076c5960ab96178083f5427e6cb9", original_line: 2, final_line: 2
@@ -144,15 +96,7 @@ s.add_test(
       path: "src/baz/qux/hello.rs",
       location: { start_line: 6, end_line: 9 },
       message: "Complexity is 1 for 3 line(s) of code at print_hello.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 12,
-        PARAM: 0,
-        length: 4,
-        function: "print_hello",
-        long_name: "print_hello"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "7e4c807c0610e658f65874ffbef8ee7e44914731", original_line: 6, final_line: 6
@@ -163,15 +107,7 @@ s.add_test(
       path: "src/foo/bar/hello.m",
       location: { start_line: 3, end_line: 8 },
       message: "Complexity is 1 for 5 line(s) of code at hello_world.",
-      object: {
-        NLOC: 5,
-        CCN: 1,
-        token: 15,
-        PARAM: 0,
-        length: 6,
-        function: "hello_world",
-        long_name: "hello_world()"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "5e4df70cbe0a97dffc6984e47f2425dc1b5ef306", original_line: 3, final_line: 3
@@ -182,15 +118,7 @@ s.add_test(
       path: "src/foo/bar/hello.m",
       location: { start_line: 10, end_line: 13 },
       message: "Complexity is 1 for 4 line(s) of code at main.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 21,
-        PARAM: 1,
-        length: 4,
-        function: "main",
-        long_name: "main( int argc , const char * argv [ ])"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "7ed115c44fba1afccd67bc57ac1ab3dead320c12", original_line: 10, final_line: 10
@@ -201,15 +129,7 @@ s.add_test(
       path: "src/foo/bar/hello.swift",
       location: { start_line: 1, end_line: 4 },
       message: "Complexity is 1 for 3 line(s) of code at hello_world.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 10,
-        PARAM: 0,
-        length: 4,
-        function: "hello_world",
-        long_name: "hello_world"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "de8a54f94e2c7fa83cb8713e423cfcf77f705fb4", original_line: 1, final_line: 1
@@ -220,15 +140,7 @@ s.add_test(
       path: "src/foo/hello.c",
       location: { start_line: 6, end_line: 10 },
       message: "Complexity is 1 for 5 line(s) of code at main.",
-      object: {
-        NLOC: 5,
-        CCN: 1,
-        token: 13,
-        PARAM: 1,
-        length: 5,
-        function: "main",
-        long_name: "main( void)"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "cf51d105d80da8bcee55efeb3b70011502abf9d0", original_line: 6, final_line: 6
@@ -239,15 +151,7 @@ s.add_test(
       path: "src/foo/hello.c",
       location: { start_line: 12, end_line: 16 },
       message: "Complexity is 1 for 4 line(s) of code at print_hello_world.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 10,
-        PARAM: 0,
-        length: 5,
-        function: "print_hello_world",
-        long_name: "print_hello_world()"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "64218a0fd55b3bf3c6e2231b24cb97b4beb7c07d", original_line: 12, final_line: 12
@@ -258,15 +162,7 @@ s.add_test(
       path: "src/foo/hello.cpp",
       location: { start_line: 6, end_line: 10 },
       message: "Complexity is 1 for 5 line(s) of code at main.",
-      object: {
-        NLOC: 5,
-        CCN: 1,
-        token: 13,
-        PARAM: 1,
-        length: 5,
-        function: "main",
-        long_name: "main( void)"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "cf51d105d80da8bcee55efeb3b70011502abf9d0", original_line: 6, final_line: 6
@@ -277,15 +173,7 @@ s.add_test(
       path: "src/foo/hello.cpp",
       location: { start_line: 12, end_line: 16 },
       message: "Complexity is 1 for 4 line(s) of code at print_hello_world.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 13,
-        PARAM: 0,
-        length: 5,
-        function: "print_hello_world",
-        long_name: "print_hello_world()"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "64218a0fd55b3bf3c6e2231b24cb97b4beb7c07d", original_line: 12, final_line: 12
@@ -296,15 +184,7 @@ s.add_test(
       path: "src/foo/hello.java",
       location: { start_line: 2, end_line: 4 },
       message: "Complexity is 1 for 3 line(s) of code at HelloWorld::main.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 13,
-        PARAM: 0,
-        length: 3,
-        function: "HelloWorld::main",
-        long_name: "HelloWorld::main( String args [ ])"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "147eb5fd9cd9f1e6d7d48e4fbd89e0dd53b13c0c", original_line: 2, final_line: 2
@@ -315,15 +195,7 @@ s.add_test(
       path: "src/foo/hello.java",
       location: { start_line: 6, end_line: 9 },
       message: "Complexity is 1 for 3 line(s) of code at HelloWorld::printHelloWorld.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 14,
-        PARAM: 0,
-        length: 4,
-        function: "HelloWorld::printHelloWorld",
-        long_name: "HelloWorld::printHelloWorld()"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "85f8de94a8f46916f827884dbb7f798dbe79fc95", original_line: 6, final_line: 6
@@ -334,15 +206,7 @@ s.add_test(
       path: "src/foo/hello.js",
       location: { start_line: 1, end_line: 4 },
       message: "Complexity is 1 for 3 line(s) of code at hello_world.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 11,
-        PARAM: 0,
-        length: 4,
-        function: "hello_world",
-        long_name: "hello_world ( )"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "d8a4f7e4e1e40d4d89907f7c88e7910b5f4c89fb", original_line: 1, final_line: 1
@@ -380,15 +244,7 @@ s.add_test(
       path: "src/baz/qux/今日は世界.go",
       location: { start_line: 9, end_line: 12 },
       message: "Complexity is 1 for 3 line(s) of code at hello_world.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 12,
-        PARAM: 0,
-        length: 4,
-        function: "hello_world",
-        long_name: "hello_world"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "de8a54f94e2c7fa83cb8713e423cfcf77f705fb4", original_line: 9, final_line: 9
@@ -399,15 +255,7 @@ s.add_test(
       path: "src/baz/こんにちは.py",
       location: { start_line: 1, end_line: 3 },
       message: "Complexity is 1 for 2 line(s) of code at hello_world.",
-      object: {
-        NLOC: 2,
-        CCN: 1,
-        token: 8,
-        PARAM: 0,
-        length: 3,
-        function: "hello_world",
-        long_name: "hello_world( )"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "a3a7f8f418d186dcf9849a0109ed2cfb1c24c0c6", original_line: 1, final_line: 1
@@ -418,15 +266,7 @@ s.add_test(
       path: "src/foo/こんにちは世界.cs",
       location: { start_line: 4, end_line: 7 },
       message: "Complexity is 1 for 4 line(s) of code at HelloWorld::Main.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 9,
-        PARAM: 0,
-        length: 4,
-        function: "HelloWorld::Main",
-        long_name: "HelloWorld::Main()"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "6f8675952e222798e307e7e02f8df48be9afa3d2", original_line: 4, final_line: 4
@@ -437,15 +277,7 @@ s.add_test(
       path: "src/foo/こんにちは世界.cs",
       location: { start_line: 9, end_line: 13 },
       message: "Complexity is 1 for 4 line(s) of code at HelloWorld::PrintHelloWorld.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 14,
-        PARAM: 0,
-        length: 5,
-        function: "HelloWorld::PrintHelloWorld",
-        long_name: "HelloWorld::PrintHelloWorld()"
-      },
+      object: :_,
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "971fa2c643e4f0d82408f8d118cd0d33fdf98267", original_line: 9, final_line: 9
