fce3fc87d8096f8549d52f1097eef31695c0d87b
suetsuguk <kentarosuetsugu@gmail.com>
2020-11-13 13:40:23 +0900

    Divide target files into two tests; success and root_dir

--- test/smokes/lizard/expectations.rb
+++ test/smokes/lizard/expectations.rb
@@ -65,25 +65,6 @@ s.add_test(
     },
     {
       id: "code-metrics",
-      path: "src/baz/hello.py",
-      location: { start_line: 1, end_line: 3 },
-      message: "Complexity is 1 for 2 line(s) of code at hello_world.",
-      object: {
-        NLOC: 2,
-        CCN: 1,
-        token: 8,
-        PARAM: 0,
-        length: 3,
-        function: "hello_world",
-        long_name: "hello_world( )"
-      },
-      links: [],
-      git_blame_info: {
-        commit: :_, line_hash: "a3a7f8f418d186dcf9849a0109ed2cfb1c24c0c6", original_line: 1, final_line: 1
-      }
-    },
-    {
-      id: "code-metrics",
       path: "src/baz/hello.rb",
       location: { start_line: 1, end_line: 4 },
       message: "Complexity is 1 for 3 line(s) of code at hello_world.",
@@ -118,44 +99,6 @@ s.add_test(
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "6bbc59d8a3b8d34f9a4b6452b7ec62242797bfec", original_line: 2, final_line: 2
-      }
-    },
-    {
-      id: "code-metrics",
-      path: "src/baz/qux/hello.go",
-      location: { start_line: 5, end_line: 7 },
-      message: "Complexity is 1 for 3 line(s) of code at main.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 9,
-        PARAM: 0,
-        length: 3,
-        function: "main",
-        long_name: "main"
-      },
-      links: [],
-      git_blame_info: {
-        commit: :_, line_hash: "5656b6543b24b9b164b717943f7e56d1cf5954c0", original_line: 5, final_line: 5
-      }
-    },
-    {
-      id: "code-metrics",
-      path: "src/baz/qux/hello.go",
-      location: { start_line: 9, end_line: 12 },
-      message: "Complexity is 1 for 3 line(s) of code at hello_world.",
-      object: {
-        NLOC: 3,
-        CCN: 1,
-        token: 12,
-        PARAM: 0,
-        length: 4,
-        function: "hello_world",
-        long_name: "hello_world"
-      },
-      links: [],
-      git_blame_info: {
-        commit: :_, line_hash: "de8a54f94e2c7fa83cb8713e423cfcf77f705fb4", original_line: 9, final_line: 9
       }
     },
     {
@@ -350,44 +293,6 @@ s.add_test(
     },
     {
       id: "code-metrics",
-      path: "src/foo/hello.cs",
-      location: { start_line: 4, end_line: 7 },
-      message: "Complexity is 1 for 4 line(s) of code at HelloWorld::Main.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 9,
-        PARAM: 0,
-        length: 4,
-        function: "HelloWorld::Main",
-        long_name: "HelloWorld::Main()"
-      },
-      links: [],
-      git_blame_info: {
-        commit: :_, line_hash: "6f8675952e222798e307e7e02f8df48be9afa3d2", original_line: 4, final_line: 4
-      }
-    },
-    {
-      id: "code-metrics",
-      path: "src/foo/hello.cs",
-      location: { start_line: 9, end_line: 13 },
-      message: "Complexity is 1 for 4 line(s) of code at HelloWorld::PrintHelloWorld.",
-      object: {
-        NLOC: 4,
-        CCN: 1,
-        token: 14,
-        PARAM: 0,
-        length: 5,
-        function: "HelloWorld::PrintHelloWorld",
-        long_name: "HelloWorld::PrintHelloWorld()"
-      },
-      links: [],
-      git_blame_info: {
-        commit: :_, line_hash: "971fa2c643e4f0d82408f8d118cd0d33fdf98267", original_line: 9, final_line: 9
-      }
-    },
-    {
-      id: "code-metrics",
       path: "src/foo/hello.java",
       location: { start_line: 2, end_line: 4 },
       message: "Complexity is 1 for 3 line(s) of code at HelloWorld::main.",
@@ -441,6 +346,109 @@ s.add_test(
       links: [],
       git_blame_info: {
         commit: :_, line_hash: "d8a4f7e4e1e40d4d89907f7c88e7910b5f4c89fb", original_line: 1, final_line: 1
+      }
+    }
+  ],
+  analyzer: { name: "Lizard", version: default_version }
+)
+
+s.add_test(
+  "root_dir",
+  type: "success",
+  issues: [
+    {
+      id: "code-metrics",
+      path: "src/baz/qux/今日は世界.go",
+      location: { start_line: 5, end_line: 7 },
+      message: "Complexity is 1 for 3 line(s) of code at main.",
+      object: {
+        NLOC: 3,
+        CCN: 1,
+        token: 9,
+        PARAM: 0,
+        length: 3,
+        function: "main",
+        long_name: "main"
+      },
+      links: [],
+      git_blame_info: {
+        commit: :_, line_hash: "5656b6543b24b9b164b717943f7e56d1cf5954c0", original_line: 5, final_line: 5
+      }
+    },
+    {
+      id: "code-metrics",
+      path: "src/baz/qux/今日は世界.go",
+      location: { start_line: 9, end_line: 12 },
+      message: "Complexity is 1 for 3 line(s) of code at hello_world.",
+      object: {
+        NLOC: 3,
+        CCN: 1,
+        token: 12,
+        PARAM: 0,
+        length: 4,
+        function: "hello_world",
+        long_name: "hello_world"
+      },
+      links: [],
+      git_blame_info: {
+        commit: :_, line_hash: "de8a54f94e2c7fa83cb8713e423cfcf77f705fb4", original_line: 9, final_line: 9
+      }
+    },
+    {
+      id: "code-metrics",
+      path: "src/baz/こんにちは.py",
+      location: { start_line: 1, end_line: 3 },
+      message: "Complexity is 1 for 2 line(s) of code at hello_world.",
+      object: {
+        NLOC: 2,
+        CCN: 1,
+        token: 8,
+        PARAM: 0,
+        length: 3,
+        function: "hello_world",
+        long_name: "hello_world( )"
+      },
+      links: [],
+      git_blame_info: {
+        commit: :_, line_hash: "a3a7f8f418d186dcf9849a0109ed2cfb1c24c0c6", original_line: 1, final_line: 1
+      }
+    },
+    {
+      id: "code-metrics",
+      path: "src/foo/こんにちは世界.cs",
+      location: { start_line: 4, end_line: 7 },
+      message: "Complexity is 1 for 4 line(s) of code at HelloWorld::Main.",
+      object: {
+        NLOC: 4,
+        CCN: 1,
+        token: 9,
+        PARAM: 0,
+        length: 4,
+        function: "HelloWorld::Main",
+        long_name: "HelloWorld::Main()"
+      },
+      links: [],
+      git_blame_info: {
+        commit: :_, line_hash: "6f8675952e222798e307e7e02f8df48be9afa3d2", original_line: 4, final_line: 4
+      }
+    },
+    {
+      id: "code-metrics",
+      path: "src/foo/こんにちは世界.cs",
+      location: { start_line: 9, end_line: 13 },
+      message: "Complexity is 1 for 4 line(s) of code at HelloWorld::PrintHelloWorld.",
+      object: {
+        NLOC: 4,
+        CCN: 1,
+        token: 14,
+        PARAM: 0,
+        length: 5,
+        function: "HelloWorld::PrintHelloWorld",
+        long_name: "HelloWorld::PrintHelloWorld()"
+      },
+      links: [],
+      git_blame_info: {
+        commit: :_, line_hash: "971fa2c643e4f0d82408f8d118cd0d33fdf98267", original_line: 9, final_line: 9
       }
     }
   ],
