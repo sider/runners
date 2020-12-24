6cb6219da16caf2b97a9be55101cbeecfe5ebd6c
suetsuguk <kentarosuetsugu@gmail.com>
2020-12-24 02:41:06 +0000

    Raise error if expected value is nil

--- lib/runners/processor/metrics_complexity.rb
+++ lib/runners/processor/metrics_complexity.rb
@@ -27,20 +27,26 @@ module Runners
         '--output_file', report_file,
         '.')
 
-      file_issues = construct_file_issue(read_report_xml)
+      file_issues = construct_file_issues(read_report_xml)
 
       Results::Success.new(guid: guid, analyzer: analyzer, issues: file_issues)
     end
 
     private
 
-    def construct_file_issue(xml_root)
+    def construct_file_issues(xml_root)
       issues = []
 
       xml_root.each_element('//cppncss/measure[@type="File"]/item') do |elem|
-        filepath = relative_path(elem.attributes['name'])
-        sum_of_CCN = Integer(elem.elements[3].text)
-        functions = Integer(elem.elements[4].text)
+        filepath_attr = elem[:name] or raise "Required name: #{elem.inspect}"
+        filepath = relative_path(filepath_attr)
+
+        ccn_text = elem.elements[3]&.text or raise "Required text: #{elem.elements[3].inspect}"
+        sum_of_CCN = Integer(ccn_text)
+
+        functions_text = elem.elements[4]&.text or raise "Required text: #{elem.elements[4].inspect}"
+        functions = Integer(functions_text)
+
         msg = "The sum of complexity of total #{functions} function(s) is #{sum_of_CCN}."
 
         issues << Issue.new(
