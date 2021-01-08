sider/runners

commit a182f02bb972f64757f533f8adbcd245f53436c9

Use always Yarn v1 even if `.yarnrc` exists (#1876)

lib/runners/nodejs.rb
@@ -95,7 +95,7 @@ module Runners
     def show_runtime_versions
       capture3! "node", "-v"
       capture3! "npm", "-v"
-      capture3! "yarn", "-v"
+      run_yarn "-v"
     end
 
     private
@@ -166,7 +166,15 @@ module Runners
       end
     end
 
-    # @see https://yarnpkg.com/en/docs/cli/install
+    def run_yarn(arg, *args)
+      # NOTE: Avoid the effects of `.yarnrc` or `.yarnrc.yml`.
+      #
+      # @see https://classic.yarnpkg.com/en/docs/yarnrc/#toc-yarn-path
+      # @see https://yarnpkg.com/configuration/yarnrc#yarnPath
+      capture3! "yarn", "--no-default-rc", arg, *args
+    end
+
+    # @see https://classic.yarnpkg.com/en/docs/cli/install
     def yarn_install(option)
       cli_options = %w[
         --ignore-engines
@@ -193,7 +201,7 @@ module Runners
       end
 
       begin
-        capture3_with_retry! "yarn", "install", *cli_options
+        run_yarn "install", *cli_options
       rescue Shell::ExecError
         message = "`yarn install` failed. Please confirm `yarn.lock` is consistent with `package.json`."
         trace_writer.error message

test/nodejs_test.rb
@@ -426,23 +426,23 @@ class NodejsTest < Minitest::Test
       FileUtils.cp data("yarn.lock"), processor.yarn_lock_path
 
       processor.send(:yarn_install, INSTALL_OPTION_NONE)
-      refute eslint.exist?
+      refute_path_exists eslint
 
       processor.send(:yarn_install, INSTALL_OPTION_ALL)
-      assert eslint.exist?
+      assert_path_exists eslint
 
       eslint.rmtree
       processor.send(:yarn_install, INSTALL_OPTION_PRODUCTION)
-      assert eslint.exist?
+      assert_path_exists eslint
 
       node_modules.rmtree
       processor.send(:yarn_install, INSTALL_OPTION_DEVELOPMENT)
-      assert eslint.exist?
+      assert_path_exists eslint
 
       expected_commands = [
-        %w[yarn install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
-        %w[yarn install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile --production],
-        %w[yarn install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
+        %w[yarn --no-default-rc install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
+        %w[yarn --no-default-rc install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile --production],
+        %w[yarn --no-default-rc install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
       ]
       assert_equal expected_commands, actual_commands
 

sig/runners/nodejs.rbs
@@ -51,6 +51,8 @@ module Runners
 
     def npm_install: (install_option) -> void
 
+    def run_yarn: (String, *String) -> [String, String]
+
     def yarn_install: (install_option) -> void
 
     def list_installed_nodejs_deps: (?only: Array[String], ?chdir: Pathname | String | nil) -> Hash[String, String]

test/smokes/eslint/expectations.rb
@@ -487,3 +487,17 @@ s.add_test(
   analyzer: { name: "ESLint", version: default_version },
   config_file: "sider.yml"
 )
+
+s.add_test(
+  "yarnrc",
+  type: "success",
+  issues: [],
+  analyzer: { name: "ESLint", version: "7.14.0" }
+)
+
+s.add_test(
+  "yarnrc_yml",
+  type: "success",
+  issues: [],
+  analyzer: { name: "ESLint", version: "7.13.0" }
+)

test/smokes/eslint/yarnrc/.yarnrc
@@ -1,0 +1,1 @@
+yarn-path "./yarn"

test/smokes/eslint/yarnrc/yarn.js
@@ -1,0 +1,5 @@
+if (process.argv.includes("-v")) {
+  console.log("Dummy Yarn 2.0.0")
+} else {
+  throw new Error("Dummy Yarn failed!")
+}

test/smokes/eslint/yarnrc_yml/.yarnrc.yml
@@ -1,0 +1,1 @@
+yarnPath: "./yarn"

test/smokes/eslint/yarnrc_yml/yarn.js
@@ -1,0 +1,5 @@
+if (process.argv.includes("-v")) {
+  console.log("Dummy Yarn 2.0.0")
+} else {
+  throw new Error("Dummy Yarn failed!")
+}

commit 161f0002feb9c28db508c0087a5d229a57a9e9b1

Do not use `--no-default-rc` flag for `yarn`
This change aims to fix a bug about the `--no-default-rc` flag of Yarn (introduced with #1876).

If using `--no-default-rc`, Yarn ignores any auto tokens in `.yarnrc` or `.npmrc` files.
This means that we **cannot install private packages** via auth tokens like FontAwesome.

```console
$ yarn --help | grep no-default-rc
    --no-default-rc                     prevent Yarn from automatically detecting yarnrc and npmrc files
```

Thus, this change moves yarnrc files temporarily instead of using `--no-default-rc`.

lib/runners/nodejs.rb
@@ -171,7 +171,22 @@ module Runners
       #
       # @see https://classic.yarnpkg.com/en/docs/yarnrc/#toc-yarn-path
       # @see https://yarnpkg.com/configuration/yarnrc#yarnPath
-      capture3! "yarn", "--no-default-rc", arg, *args
+      yarnrc_files = [".yarnrc", ".yarnrc.yml", ".yarnrc.yaml"].filter_map do |file|
+        src = current_dir / file
+        src if src.exist?
+      end
+
+      mktmpdir do |backup_dir|
+        backup_files = yarnrc_files.map do |src|
+          (backup_dir / src.basename).tap { |dst| src.rename(dst) }
+        end
+
+        begin
+          capture3! "yarn", arg, *args
+        ensure
+          backup_files.each { |file| file.rename(current_dir / file.basename) }
+        end
+      end
     end
 
     # @see https://classic.yarnpkg.com/en/docs/cli/install

test/nodejs_test.rb
@@ -426,27 +426,42 @@ class NodejsTest < Minitest::Test
       node_modules = workspace.working_dir / "node_modules"
       eslint = node_modules / "eslint"
 
+      yarnrc = (workspace.working_dir / ".yarnrc").tap { _1.write 'yarn-path "foo"' }
+      yarnrc_yml = (workspace.working_dir / ".yarnrc.yml").tap { _1.write 'yarnPath: "foo"' }
+      yarnrc_yaml = (workspace.working_dir / ".yarnrc.yaml").tap { _1.write 'yarnPath: "foo"' }
+
       processor.package_json_path.write(JSON.generate(dependencies: { "eslint" => "6.0.1" }))
       FileUtils.cp data("yarn.lock"), processor.yarn_lock_path
 
       processor.send(:yarn_install, INSTALL_OPTION_NONE)
       refute_path_exists eslint
+      assert_path_exists yarnrc
+      assert_path_exists yarnrc_yml
+      assert_path_exists yarnrc_yaml
 
       processor.send(:yarn_install, INSTALL_OPTION_ALL)
       assert_path_exists eslint
+      assert_path_exists yarnrc
+      assert_path_exists yarnrc_yml
+      assert_path_exists yarnrc_yaml
 
       eslint.rmtree
+      yarnrc_yml.rmtree
+      yarnrc_yaml.rmtree
       processor.send(:yarn_install, INSTALL_OPTION_PRODUCTION)
       assert_path_exists eslint
+      assert_path_exists yarnrc
+      refute_path_exists yarnrc_yml
+      refute_path_exists yarnrc_yaml
 
       node_modules.rmtree
       processor.send(:yarn_install, INSTALL_OPTION_DEVELOPMENT)
       assert_path_exists eslint
 
       expected_commands = [
-        %w[yarn --no-default-rc install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
-        %w[yarn --no-default-rc install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile --production],
-        %w[yarn --no-default-rc install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
+        %w[yarn install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
+        %w[yarn install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile --production],
+        %w[yarn install --ignore-engines --ignore-scripts --no-progress --non-interactive --frozen-lockfile],
       ]
       assert_equal expected_commands, actual_commands
 