71f26fcce71a29f2e8f6fc61d02bb880b7389120
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-02-02 22:37:09 +0900

    [Slim-Lint] New support
    Close #1720

---
+++ lib/runners/processor/slim_lint.rb
@@ -0,0 +1,122 @@
+module Runners
+  class Processor::SlimLint < Processor
+    include Ruby
+    include RuboCopUtils
+
+    Schema = _ = StrongJSON.new do
+      # @type self: SchemaClass
+
+      let :runner_config, Schema::BaseConfig.ruby.update_fields { |fields|
+        fields.merge!({
+          target: enum?(string, array(string)),
+          config: string?,
+        })
+      }
+
+      let :issue, object(
+        severity: string,
+      )
+    end
+
+    register_config_schema(name: :slim_lint, schema: Schema.runner_config)
+
+    GEM_NAME = "slim_lint".freeze
+    REQUIRED_GEM_NAMES = ["rubocop"].freeze
+    CONSTRAINTS = {
+      GEM_NAME => [">= 0.20.2", "< 1.0.0"]
+    }.freeze
+    DEFAULT_TARGET = ".".freeze
+    MISSING_ID = "missing-ID".freeze
+
+    def analyzer_bin
+      "slim-lint"
+    end
+
+    def setup
+      default_gems = default_gem_specs(GEM_NAME, *REQUIRED_GEM_NAMES)
+
+      if setup_default_rubocop_config
+        # NOTE: See `Processor::RuboCop` about no versions.
+        default_gems << GemInstaller::Spec.new(name: "meowcop", version: [])
+      end
+
+      optionals = official_rubocop_plugins + third_party_rubocop_plugins
+      install_gems(default_gems, optionals: optionals, constraints: CONSTRAINTS) { yield }
+    rescue InstallGemsFailure => exn
+      trace_writer.error exn.message
+      return Results::Failure.new(guid: guid, message: exn.message)
+    end
+
+    def analyze(_changes)
+      cmd = ruby_analyzer_command(
+        *(config_linter[:config].then { |config| config ? ["--config", config] : [] }),
+        *Array(config_linter[:target] || DEFAULT_TARGET),
+      )
+
+      stdout, _, status = capture3(cmd.bin, *cmd.args)
+
+      # @see https://github.com/ybiquitous/slim-lint/blob/v0.20.2/lib/slim_lint/cli.rb#L93
+      if [0, 65].include? status.exitstatus
+        Results::Success.new(guid: guid, analyzer: analyzer).tap do |result|
+          parse_result(stdout) do |issue|
+            result.add_issue(issue)
+          end
+        end
+      else
+        raise
+      end
+    end
+
+    private
+
+    def parse_result(output)
+      # @see https://github.com/ybiquitous/slim-lint/blob/v0.20.2/lib/slim_lint/reporter/default_reporter.rb
+      pattern = /^(.+):(\d+) \[(E|W)\] (?:(.+): )?(.+)$/
+      severities = { "E" => "error", "W" => "warning" }
+
+      output.scan(pattern) do |path, line, severity, id, message|
+        path.is_a?(String) or raise
+        line.is_a?(String) or raise
+        severity.is_a?(String) or raise
+        message.is_a?(String) or raise
+
+        issue_id = build_id(id)
+
+        yield Issue.new(
+          path: relative_path(path),
+          location: Location.new(start_line: line),
+          id: issue_id,
+          message: message,
+          links: build_links(issue_id),
+          object: {
+            severity: severities.fetch(severity),
+          },
+          schema: Schema.issue,
+        )
+      end
+    end
+
+    def build_id(id)
+      case
+      when id.nil?
+        MISSING_ID
+      when id.start_with?("RuboCop:")
+        id.delete(" ")
+      else
+        id
+      end
+    end
+
+    def build_links(id)
+      case id
+      when nil, MISSING_ID
+        []
+      when /\ARuboCop:/
+        cop_name = id.delete_prefix("RuboCop:")
+        ["#{analyzer_github}/blob/v#{analyzer_version}/lib/slim_lint/linter/README.md#rubocop"] + build_rubocop_links(cop_name)
+      else
+        ["#{analyzer_github}/blob/v#{analyzer_version}/lib/slim_lint/linter/README.md##{id.downcase}"]
+      end
+    end
+  end
+end
