68f9541f3d00b1b20461544ed8f4410ed71d2646
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-02-05 17:20:24 +0900

    Unindent expected YAML

--- test/config_generator_test.rb
+++ test/config_generator_test.rb
@@ -8,68 +8,68 @@ class ConfigGeneratorTest < Minitest::Test
   end
 
   def test_generate
-    assert_yaml <<~YAML, @subject.generate
-      ## This is a configuration file to customize code analysis by Sider.
-      ##
-      ## For more information, see the documentation:
-      ## https://help.sider.review/getting-started/custom-configuration
+    assert_yaml <<-YAML, @subject.generate
+## This is a configuration file to customize code analysis by Sider.
+##
+## For more information, see the documentation:
+## https://help.sider.review/getting-started/custom-configuration
 
-      ## Customize each tools. For example:
-      # linter:
+## Customize each tools. For example:
+# linter:
 
-      ## Ignore specific files. For example:
-      # ignore:
-      #   - "*.pdf"
-      #   - "*.mp4"
-      #   - "*.min.*"
-      #   - "images/**"
+## Ignore specific files. For example:
+# ignore:
+#   - "*.pdf"
+#   - "*.mp4"
+#   - "*.min.*"
+#   - "images/**"
 
-      ## Exclude specific branches. For example:
-      # branches:
-      #   exclude:
-      #     - master
-      #     - development
-      #     - /^release-.*$/
+## Exclude specific branches. For example:
+# branches:
+#   exclude:
+#     - master
+#     - development
+#     - /^release-.*$/
     YAML
   end
 
   def test_generate_with_tools
-    assert_yaml <<~YAML, @subject.generate(tools: [:eslint])
-      ## This is a configuration file to customize code analysis by Sider.
-      ##
-      ## For more information, see the documentation:
-      ## https://help.sider.review/getting-started/custom-configuration
+    assert_yaml <<-YAML, @subject.generate(tools: [:eslint])
+## This is a configuration file to customize code analysis by Sider.
+##
+## For more information, see the documentation:
+## https://help.sider.review/getting-started/custom-configuration
 
-      ## Customize each tools. For example:
-      # linter:
-        ## ESLint configuration. See: https://help.sider.review/tools/javascript/eslint
-        # eslint:
-        #   root_dir: frontend/
-        #   npm_install: false
-        #   target:
-        #     - src/
-        #     - lib/
-        #   ext: ".js,.jsx"
-        #   config: config/.eslintrc.js
-        #   ignore-path: config/.eslintignore
-        #   ignore-pattern: vendor/**
-        #   no-ignore: true
-        #   global: require,exports:true
-        #   quiet: true
+## Customize each tools. For example:
+# linter:
+  ## ESLint configuration. See: https://help.sider.review/tools/javascript/eslint
+  # eslint:
+  #   root_dir: frontend/
+  #   npm_install: false
+  #   target:
+  #     - src/
+  #     - lib/
+  #   ext: ".js,.jsx"
+  #   config: config/.eslintrc.js
+  #   ignore-path: config/.eslintignore
+  #   ignore-pattern: vendor/**
+  #   no-ignore: true
+  #   global: require,exports:true
+  #   quiet: true
 
-      ## Ignore specific files. For example:
-      # ignore:
-      #   - "*.pdf"
-      #   - "*.mp4"
-      #   - "*.min.*"
-      #   - "images/**"
+## Ignore specific files. For example:
+# ignore:
+#   - "*.pdf"
+#   - "*.mp4"
+#   - "*.min.*"
+#   - "images/**"
 
-      ## Exclude specific branches. For example:
-      # branches:
-      #   exclude:
-      #     - master
-      #     - development
-      #     - /^release-.*$/
+## Exclude specific branches. For example:
+# branches:
+#   exclude:
+#     - master
+#     - development
+#     - /^release-.*$/
     YAML
   end
 
