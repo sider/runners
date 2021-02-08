c66772bc25f0bd0325f8adaba3ef319112df15ac
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-02-08 15:53:23 +0900

    Improve test case

--- test/config_generator_test.rb
+++ test/config_generator_test.rb
@@ -8,11 +8,13 @@ class ConfigGeneratorTest < Minitest::Test
   end
 
   def test_generate
-    assert_yaml data("#{__method__}.yml").read, @subject.generate
+    assert_yaml data("test_generate.yml").read,
+                @subject.generate
   end
 
   def test_generate_with_tools
-    assert_yaml data("#{__method__}.yml").read, @subject.generate(tools: [:brakeman, :checkstyle, :eslint])
+    assert_yaml data("test_generate_with_tools.yml").read,
+                @subject.generate(tools: [:brakeman, :checkstyle, :eslint])
   end
 
   private
@@ -21,13 +23,23 @@ class ConfigGeneratorTest < Minitest::Test
     assert_equal expected, actual
 
     # Comment out
-    yaml = actual.lines.map.with_index(1) do |line, line_num|
+    content = actual.lines.map.with_index(1) do |line, line_num|
       if line_num <= 4 # Skip header
         line
       else
         line.delete_prefix("# ")
       end
     end.join
-    Runners::Config.new(path: "foo.yml", raw_content: yaml).validate
+
+    config = Runners::Config.new(path: "foo.yml", raw_content: content)
+    assert_equal ["*.pdf", "*.mp4", "*.min.*", "images/**"], config.content[:ignore]
+    assert_equal ["master", "development", "/^release-.*$/"], config.content[:branches][:exclude]
+
+    linter = config.content[:linter]
+    if linter
+      refute_nil linter[:brakeman]
+      refute_nil linter[:checkstyle]
+      refute_nil linter[:eslint]
+    end
   end
 end
