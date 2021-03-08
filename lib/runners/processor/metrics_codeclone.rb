6beee761fb5e4e6f180497fc9d801fb371039288
suetsuguk <kentarosuetsugu@gmail.com>
2021-03-08 03:10:43 +0000

    Report sum of duplicated lines and test it

--- lib/runners/processor/metrics_codeclone.rb
+++ lib/runners/processor/metrics_codeclone.rb
@@ -7,6 +7,7 @@ module Runners
 
       let :issue, object(
         clones: integer,
+        'total-clone-lines': integer,
       )
     end
 
@@ -57,6 +58,7 @@ module Runners
         message: msg,
         object: {
           clones: clones,
+          'total-clone-lines': sum_of_lines,
         },
         schema: Schema.issue,
       )
