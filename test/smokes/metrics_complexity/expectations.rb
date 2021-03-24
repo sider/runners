bbd8000d1748dd7926b37620cefdc1d88f8bd21a
Gaku Nakagawa <gaku@siderlabs.com>
2021-03-24 13:50:03 +0900

    Add a test case

--- test/smokes/metrics_complexity/expectations.rb
+++ test/smokes/metrics_complexity/expectations.rb
@@ -193,3 +193,20 @@ s.add_test(
   ],
   analyzer: { name: "Metrics Complexity", version: default_version }
 )
+
+s.add_test(
+  "illegal_char_in_rspec_condition",
+  type: "success",
+  issues: [
+    {
+      id: "metrics_file-complexity",
+      path: "myclass_test.rb",
+      location: nil,
+      message: :_,
+      object: :_,
+      links: [],
+      git_blame_info: nil
+    }
+  ],
+  analyzer: { name: "Metrics Complexity", version: default_version}
+)
